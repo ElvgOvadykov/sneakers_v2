df038cd9670cbd6ff372aa1a6d2c779973bcb7fd
Elvg Ovadykov <oelvg@mail.ru>
2023-01-21 21:14:16 +0300

    add png image and delete extra files

--- client/src/containers/MainPage/index.tsx
+++ client/src/containers/MainPage/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import SearchSvg from "@images/search.svg";
+import styles from "./MainPage.module.css";
 
 function MainPage() {
-    return (<div></div>)
+	return (
+		<div className={styles.root}>
+			<div className={styles.header}>
+				<h1>Все кроссовки</h1>
+				<div className={styles.searchBlock}>
+					<img src={SearchSvg} alt="Search" />
+					<input type="text" placeholder={"Поиск..."} className={styles.searchInput} />
+				</div>
+			</div>
+
+			{/* <div className={styles.sneakersList}>
+				{props.sneakers.map((item) => (
+					<SneakersCard cartItem={item} />
+				))}
+			</div> */}
+		</div>
+	);
 }
 
 export default MainPage;
